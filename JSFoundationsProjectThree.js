026104db44c74c5fc9ff8d8cf302ebbb82ea9890
unknown <a_bennakhi@hotmail.com>
2018-11-23 16:45:36 +0300

    Complete

--- JSFoundationsProjectThree.js
+++ JSFoundationsProjectThree.js
@@ -7,6 +7,8 @@
 class Person {
 	constructor(name, age) {
 		// your code goes here!
+		this._name = name;
+		this._age = age;
 	}
 }
 
@@ -21,6 +23,8 @@ class Person {
 class Instructor extends Person {
 	constructor(name, age, speakingVolume) {
 		// your code goes here!
+		super(name,age);
+		this._speakingVolume = speakingVolume;
 	}
 
 	/**
@@ -29,6 +33,12 @@ class Instructor extends Person {
 	 */
 	introduceSelf() {
 		// your code goes here!
+		var theintro = "Hello, I am your instructor, "+ this._name+". Welcome to the class!";
+		if(this._speakingVolume > 75){
+			console.log(theintro.toUpperCase() + "!!!!!");
+		} else{
+			console.log(theintro);
+		}
 	}
 }
 
@@ -41,18 +51,32 @@ class Instructor extends Person {
 class Student extends Person {
 	constructor(name, age, subjects) {
 		// your code goes here!
+		super(name, age);
+		this._subjects = subjects;
 	}
 
 	// This method should add a subject to this student's array of subjects.
 	// What if the subject already exists in this student's array of subjects?
 	addSubject(subject) {
 		// your code goes here!
+		if(this._subjects.includes(subject)){
+			console.log(this._name+" is already in this classroom.");
+		}
+		else{
+			this._subjects.push(subject);
+		}
 	}
 
 	// This method should remove a subject from this student's array of subjects.
 	// What if the subject doesn't exist in this student's array of subjects?
 	removeSubject(subject) {
 		// your code goes here!
+		if(this._subjects.includes(subject)){
+			this._subjects = this._subjects.filter(e => e !== subject);
+		}
+		else{
+			console.log(this._name+" can't be added to this class becasue they chose other subjects.");;
+		}
 	}
 }
 
@@ -70,6 +94,10 @@ class Student extends Person {
 class Classroom {
 	constructor(instructor, capacity, subject) {
 		// your code goes here!
+		this._instructor = instructor;
+		this._capacity = capacity;
+		this._subject = subject;
+		this._students = [];
 	}
 
 	/**
@@ -78,21 +106,35 @@ class Classroom {
 	 */
 	get classFull() {
 		// your code goes here!
+		if (this._capacity == this._students.length){
+			return true;
+		} else {return false;}
 	}
 
 	// This method should add a student to this classroom
 	addStudent(student) {
 		// your code goes here!
+		if(this._students.includes(student)){
+			console.log(student._name + " is already in this classroom.");
+		}else if(!student._subjects.includes(this._subject)){
+			console.log(student._name+ " can't be added to this class because they chose other subjects.");
+		}else if(this.classFull){
+			console.log("Sorry, class capacity is full.");
+		}
+		else{this._students.push(student);}
 	}
 
 	// This method should remove a student from this classroom
 	removeStudent(student) {
 		// your code goes here!
+		this._students = this._students.filter(e => e !== student);
+		
 	}
 
 	// This method should change this classroom's instructor.
 	changeInstructor(instructor) {
 		// your code goes here!
+		this._instructor = instructor;
 	}
 }
 
@@ -109,40 +151,40 @@ The following code runs the functions defined above
  * The following code will create this scenario using the classes defined above.
  */
 
-// let jennifer = new Instructor("Jennifer", 32, 30);
-// jennifer.introduceSelf();
+let jennifer = new Instructor("Jennifer", 32, 30);
+jennifer.introduceSelf();
 
-// let austin = new Instructor("Austin", 33, 80);
-// austin.introduceSelf();
+let austin = new Instructor("Austin", 33, 80);
+austin.introduceSelf();
 
-// let djangoClass = new Classroom(jennifer, 5, "Django");
-// let reactClass = new Classroom(austin, 5, "React");
+let djangoClass = new Classroom(jennifer, 5, "Django");
+let reactClass = new Classroom(austin, 5, "React");
 
-// let adam = new Student("Adam", 19, ["Django", "React"]);
-// let ghalya = new Student("Ghalya", 29, ["React"]);
-// let rashid = new Student("Rashid", 35, ["Django"]);
-// let salman = new Student("Salman", 23, ["Django"]);
-// let aya = new Student("Aya", 26, ["Django", "React"]);
-// let ali = new Student("Ali", 28, ["React"]);
-// let ahmad = new Student("Ahmad", 27, ["Django", "React"]);
-// let malak = new Student("Malak", 31, ["Django", "React"]);
+let adam = new Student("Adam", 19, ["Django", "React"]);
+let ghalya = new Student("Ghalya", 29, ["React"]);
+let rashid = new Student("Rashid", 35, ["Django"]);
+let salman = new Student("Salman", 23, ["Django"]);
+let aya = new Student("Aya", 26, ["Django", "React"]);
+let ali = new Student("Ali", 28, ["React"]);
+let ahmad = new Student("Ahmad", 27, ["Django", "React"]);
+let malak = new Student("Malak", 31, ["Django", "React"]);
 
-// reactClass.addStudent(adam)
-// reactClass.addStudent(ghalya)
-// reactClass.addStudent(Aya)
-// reactClass.addStudent(Ali)
-// reactClass.addStudent(Ahmad)
+reactClass.addStudent(adam)
+reactClass.addStudent(ghalya)
+reactClass.addStudent(aya)
+reactClass.addStudent(ali)
+reactClass.addStudent(ahmad)
 
-// djangoClass.addStudent(adam)
-// djangoClass.addStudent(rashid)
-// djangoClass.addStudent(Salman)
-// djangoClass.addStudent(Ahmad)
+djangoClass.addStudent(adam)
+djangoClass.addStudent(rashid)
+djangoClass.addStudent(salman)
+djangoClass.addStudent(ahmad)
 
 // // this one shuold log "adam is already in this classroom."
-// djangoClass.addStudent(adam);
+djangoClass.addStudent(adam);
 
 // // this one should log "ghalya can't be added to this class because they chose other subjects."
-// djangoClass.addStudent(ghalya);
+djangoClass.addStudent(ghalya);
 
 // // This one should log "Sorry, class capacity is full."
-// reactClass.addStudent(Malak)
+reactClass.addStudent(malak)
